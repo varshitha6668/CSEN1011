80f93440e13586267b6770efb09d360d29d5741a
varshitha <93367953+varshitha6668@users.noreply.github.com>
2021-12-07 14:50:54 +0530

    Add files via upload

---
+++ program on amstrong.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n,n1,r,s=0;
+	printf("enter a three digit number:");
+	scanf("%d,&n");
+	n1=n;
+	while (n1=0)
+	{
+		r+n%10;
+		s=s+(r*r*r);
+		n1/=10;
+	}
+    if(n==s);
+	}
+
+    
+	
+	
+
