4c30afdae27704b9ee5e1221488426009e264b38
varshitha <93367953+varshitha6668@users.noreply.github.com>
2021-12-21 14:52:35 +0530

    Add files via upload

---
+++ floyds triangle.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j,a=1;
+	printf("enter the number of rows:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=1;j++)
+		printf("%d",a);
+	}
+	printf("\n");
+	return 0;
+}
+
+  
